33665bcdbce018dbf0b66b11af46f9c0c6112038
Tracy Torisky <mommers1@verizon.net>
2021-10-09 17:33:27 -0400

    worked on current date

--- Develop/Assets/main.js
+++ Develop/Assets/main.js
@@ -1,2 +1,9 @@
-var today = moment();
-console.log();
+// var activityComment = {
+    // comment: comment.value
+// }
+
+// localStorage.setItem("activityComment", JSON.stringify(activityComment));
+// renderMessage();
+
+var currentDay = new Date();
+moment(currentDay).format("MMM Do, YYYY");
